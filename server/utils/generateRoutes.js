2d6a9f275cb1c2130045f5c119dbb34da104a80c
Matt Bradshaw <mattbrad@uk.ibm.com>
2018-07-18 13:31:34 +0100

    Initial project setup and using old files

---
+++ server/utils/generateRoutes.js
@@ -0,0 +1,27 @@
+const hash = require('js-hash-code');
+
+const pubs = ['syb', 'gnf', 'msn', 'swt', 'hud', 'mir', 'dew', 'btl'];
+// const pubs = ['syb', 'gnf', 'msn'];
+
+const getRoutes = (pubs) => pubs.reduce(
+        (subsets, value) => subsets.concat(
+         subsets.map(set => [...set,value])
+        ),
+        [[]]
+    );
+
+const addDirection = (arr, direction) => {
+    return arr.map((route) => ({dir:direction, route : route} ));
+}
+
+const stops = getRoutes(pubs);
+let routes = [...addDirection(stops, 'east'), ...addDirection(stops, 'west')];
+
+const lookup = {};
+routes.forEach((route) => {
+    lookup[hash(route)] = route;
+});
+
+console.log('lookup', lookup);
+
+module.exports = lookup;

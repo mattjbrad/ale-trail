da90dc3ec0f648ac37c5cccb680ddc2cd56575af
Matt Bradshaw <mattbrad@uk.ibm.com>
2018-07-27 09:33:44 +0100

    Swapping directions

--- client/src/components/Route/Direction/Direction.js
+++ client/src/components/Route/Direction/Direction.js
@@ -6,8 +6,8 @@ export default () => {
   return (
     <FormGroup>
         <p>Direction</p>
-        <Radio id='east' name='direction' value='east' label="East" />
         <Radio id='west' name='direction' value='west' label="West" />
+        <Radio id='east' name='direction' value='east' label="East" />
     </FormGroup>
   )
 }

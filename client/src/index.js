870debd943bd9b6e1142c9075e7b27f3d4501713
Matt Bradshaw <mattbrad@uk.ibm.com>
2018-07-28 14:34:23 +0100

    register serviec worker

--- client/src/index.js
+++ client/src/index.js
@@ -5,4 +5,4 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 console.log('index');
 ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+registerServiceWorker();

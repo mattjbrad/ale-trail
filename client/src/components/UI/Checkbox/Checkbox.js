ca7611d7214261ceb12d34bca30837aa0a76abed
Matt Bradshaw <mattbrad@uk.ibm.com>
2018-07-30 15:33:27 +0100

    formatting the form for small devices

--- client/src/components/UI/Checkbox/Checkbox.js
+++ client/src/components/UI/Checkbox/Checkbox.js
@@ -5,10 +5,12 @@ export default (props) => {
 	return (
 		<div className={classes.Div}>
 			<div className={classes.Labels}>
-				<label htmlFor={props.id}>
-					<span>{props.label}</span>
-					<input onChange={props.change} checked={props.checked} type="checkbox" className={classes.Checkbox} id={props.id} name={props.name} value={props.value} />
-				</label>
+			<div>
+				<label htmlFor={props.id}>{props.label}</label>
+			</div>
+			<div>
+				<input onChange={props.change} checked={props.checked} type="checkbox" className={classes.Checkbox}id={props.id} name={props.name} value={props.value} />
+			</div>
 			</div>
 		</div>
 	)
